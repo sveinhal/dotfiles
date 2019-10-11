2ac67473d0eadd40f41a5f22ffac1eefdc79ba07
Svein Halvor Halvorsen <svein@fink.no>
2019-10-11 13:30:55 +0200

    Use correct pubkey

--- system/keys.sh
+++ system/keys.sh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
