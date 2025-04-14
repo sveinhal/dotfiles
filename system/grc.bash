3bbb7dd8de426c8828784bfcd89623c978230ce3
Svein Halvor Halvorsen <svein@fink.no>
2025-04-14 10:19:09 +0200

    Colors

--- system/grc.bash
+++ system/grc.bash
@@ -1,7 +1,7 @@
 # GRC colorizes nifty unix tools all over the place
 if $(grc &>/dev/null)
 then
-  source `brew --prefix`/etc/grc.zsh
+  source `brew --prefix`/etc/grc.sh
 
   alias head="colourify head"
   alias tail="colourify tail"
--- system/grc.zsh
+++ system/grc.zsh
@@ -3,8 +3,9 @@ if $(grc &>/dev/null)
 then
   source `brew --prefix`/etc/grc.zsh
 
-  alias head="colourify head"
-  alias tail="colourify tail"
+  alias head="grc -es head"
+  alias tail="grc -es tail"
+  alias ls="grc -es /bin/ls --color"
 else
   echo "grc not installed. Install with brew install grc"
 fi
