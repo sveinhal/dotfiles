9cfbed9782a1cfe10e3680f707a01914a5af990c
Svein Halvor Halvorsen <svein@fink.no>
2025-05-05 17:12:41 +0200

    Tailscale-alias on macOS only

--- tailscale/alias.sh
+++ tailscale/alias.sh
@@ -1,1 +1,3 @@
-alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+if [ -f "/Applications/Tailscale.app/Contents/MacOS/Tailscale" ]; then
+    alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+fi
