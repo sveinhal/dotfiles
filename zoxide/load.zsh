44ab1189cffa974a00a35c466a5ee403fbbdd90a
Svein Halvor Halvorsen <svein@fink.no>
2025-05-12 14:00:10 +0200

    more lenient command location

--- zoxide/load.zsh
+++ zoxide/load.zsh
@@ -1,8 +1,6 @@
 # Zoxide is niftier "cd"
-if $(brew --prefix zoxide &>/dev/null)
-then
+if $(command -v zoxide &>/dev/null); then
   eval "$(zoxide init zsh --cmd cd)"
 else
-  echo "zoxide not installed. Install with brew install zoxide"
+  echo "zoxide not installed."
 fi
-
