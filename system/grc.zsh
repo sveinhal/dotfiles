520bdc2e26f248cd44272760f8d5c7845536ea17
Svein Halvor Halvorsen <svein@fink.no>
2025-05-12 13:31:33 +0200

    more resilient zsh install

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,11 +1,18 @@
 # GRC colorizes nifty unix tools all over the place
-if $(grc &>/dev/null)
-then
-  source `brew --prefix`/etc/grc.zsh
 
-  alias head="grc -es head"
-  alias tail="grc -es tail"
-  alias ls="grc -es /bin/ls --color"
-else
-  echo "grc not installed. Install with brew install grc"
+GRC=$(command -v grc)
+
+if [[ -z "$GRC" ]]; then 
+  echo "grc not installed."
+  return
 fi
+
+if [[ -f "/etc/grc.zsh" ]]; then
+  source /etc/grc.zsh
+elif [[ -f `brew --prefix`/etc/grc.zsh ]]; then
+  source `brew --prefix`/etc/grc.zsh
+fi
+
+alias head="grc -es head"
+alias tail="grc -es tail"
+alias ls="grc -es /bin/ls --color"
